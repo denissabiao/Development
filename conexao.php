51a6e888eb8500e2da74e40289cd217ac0d3a705
souldrinkerSX <denis.sabiao@gmail.com>
2017-05-12 09:54:00 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,25 +1,2 @@
 <?php
-/*
-$servidor_mysql = 'mysql.coopsaude.com.br';
-$nome_banco = 'coopsaude';
-$usuario = 'coopsaude';
-$senha = 'dennis69';
-try {
-    $con = new PDO("mysql:host=$servidor_mysql;dbname=$nome_banco","$usuario","$senha"); 
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit(1);
-}
-*/
-
-$servidor_mysql = 'mysql.suportebh.com.br';
-$nome_banco = 'suportebh12';
-$usuario = 'suportebh12';
-$senha = 'dennis363';
-try {
-    $con = new PDO("mysql:host=$servidor_mysql;dbname=$nome_banco","$usuario","$senha"); 
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit(1);
-}
-?>
+/*limpar espaços para funcionar o header*/
